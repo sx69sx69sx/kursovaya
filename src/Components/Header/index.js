b1ce55a300d7f987c15d598314c326f347d752e5
спиху <sp1xy@MacBook-Pro-spihu.local>
2025-02-17 01:40:30 +0300

    create homepage

---
+++ src/Components/Header/index.js
@@ -0,0 +1,24 @@
+const Header =()=>{
+return(
+<>
+<div className="header">
+    <div className="header-container">
+        <a className="main-text-header">Мужское</a>
+        <a className="main-text-header">Женское</a>
+        <a className="main-text-header">Скидки</a>
+        <a className="main-text-header">Поиск</a>
+    </div>
+    <div className="header-container">
+    <a className="text-accent-header">Шелковый путь</a>
+    </div>
+    <div className="header-container">
+    <a className="main-text-header">Войти</a>
+    <a className="main-text-header">Избранное</a>
+    <a className="main-text-header">Корзина (0)</a>
+    </div>
+</div>
+</>
+)
+
+}
+export default Header;
---
+++ src/Pages/Home/index.js
@@ -0,0 +1,40 @@
+
+const Home = () => {
+    return (
+        <>
+            <div className="yung-lean-container">
+                <div className="number-container">
+                    <p className="numbers-yunglean">112519</p>
+                    <p className="numbers-yunglean">112519</p>
+                    <p className="numbers-yunglean">112519</p>
+                    <p className="numbers-yunglean">112519</p>
+                </div>
+                <div className="number-container">
+                    <p className="numbers-yunglean">112519</p>
+                    <p className="numbers-yunglean">112519</p>
+                    <p className="numbers-yunglean">112519</p>
+                    <p className="numbers-yunglean">112519</p>
+                </div>
+                <div className="number-container">
+                    <p className="numbers-yunglean">112519</p>
+                    <p className="numbers-yunglean">112519</p>
+                    <p className="numbers-yunglean">112519</p>
+                    <p className="numbers-yunglean">112519</p>
+                </div>
+            </div>
+            <div className="blue-line">
+
+            </div>
+            <div className="category-container">
+                <img src="/img/blocktwoo.png" alt="Wings with Rider" />
+                <div className="circle-text left-circle">МУЖСКОЕ</div>
+                <div className="circle-text right-circle">ЖЕНСКОЕ</div>
+            </div>
+            <div className="blue-line">
+
+            </div>
+        </>
+    )
+
+}
+export default Home;
