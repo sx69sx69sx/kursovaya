c17f2a0b03b70abbe75d7db603b459966c205e20
sx69sx69sx <sp1xx454@gmail.com>
2025-02-17 23:20:04 +0300

    add another block

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -30,8 +30,18 @@ const Home = () => {
                 <div className="circle-text left-circle">МУЖСКОЕ</div>
                 <div className="circle-text right-circle">ЖЕНСКОЕ</div>
             </div>
-            <div className="blue-line">
-
+            <div className="blue-line-two">
+            <a className="text-accent-archive">Архив</a>
+            </div>
+            <div className="archive-homepage">
+                
+                <img src="/img/Group8.png"/>
+                <img src="/img/2.webp"/>
+                <img src="/img/3.webp"/>
+            </div>
+            <div className="blue-line-footer">
+                <p>ШЕЛКОВЫЙ ПУТЬ 2025</p>
+                <p>ЕЖЕДНЕВНО 11:00-19:00 </p>
             </div>
         </>
     )
